20638062cc944e636b6f9de2b1c8776c6ac3a122
Arnaldo <arnaldog@gmail.com>
2013-03-14 01:13:19 -0400

    another bug fixed

--- lib/generators/postgis/postgis_generator.rb
+++ lib/generators/postgis/postgis_generator.rb
@@ -1,7 +1,7 @@
 class PostgisGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   argument :app_name, :type => :string, :default => 'app'
-  argument :port, :type => :integer, :default => 5432
+  argument :port, :type => :string, :default => "5432"
   argument :encoding, :type => :string, :default => 'UTF-8'
   argument :username, :type => :string, :default => 'postgres'
   argument :password, :type => :string, :default => 'admin'
