dcac0aecb29cdebad6964f019b6a328f8cbf92c8
Arnaldo <arnaldog@gmail.com>
2013-03-18 17:29:39 -0400

    wena

--- lib/generators/postgis/postgis_generator.rb
+++ lib/generators/postgis/postgis_generator.rb
@@ -1,11 +1,12 @@
-class PostgisGenerator < Rails::Generators::Base
+lass PostgisGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   argument :app_name, :type => :string, :default => 'app'
-  argument :port, :type => :string, :default => "5432"
+  argument :port, :type => :string, :default => ENV['PGPORT'] || "5432"
   argument :encoding, :type => :string, :default => 'UTF-8'
-  argument :username, :type => :string, :default => 'postgres'
-  argument :password, :type => :string, :default => 'admin'
-  argument :template, :type => :string, :default => 'template_postgis'
+  argument :username, :type => :string, :default => ENV['PGUSER'] || 'postgres'
+  argument :password, :type => :string, :default => ENV['PGPASSWORD'] || 'admin'
+  argument :template_postgis, :type => :string, :default => 'template_postgis'
+  argument :pg_path, :type => :string, :default => "#{ENV['POSTGRES']}/share/postgresql/contrib/postgis-1.5"
 
   def generate_postgis
     template "database.yml", "config/database.yml", :force => true
